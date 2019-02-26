6fb72de309207f3ca41fd7c1e70de4bb12a5d70f
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2019-02-26 02:30:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-var api = 'http://superblockchain.con-ip.com:35223';
-var donationAddress = "btndEaft61oABpYFBZzLohYBEPSRduV3bMa2LX9mEjcmJJjFwFBVD7TPBg45aTQyYjHxfG9DHvCmXfLLMN8ubiJ46GrxcXS5s5";
+var api = 'http://pool.bitnovex.com:35223';
+var donationAddress = "btndAndkYgQHdwxwTi48eF1GdvRR4PsYy9RGXn231pt6fcbKnqm6uALD3R9ZNMAkQGAVvYL44Gvf2ATvdGTEqiJm3DrL3RxAYg";
 var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 1000000;  // enter in the amount of atomic units in 1 coin, eg. 1000000 shells = 1 BTNX
 var totalSupply =  100000000000000; // enter the total supply in atomic units
@@ -9,15 +9,15 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "btnx": [
-  ["superblockchain.con-ip.com/btnx", "https://superblockchain.con-ip.com:8887"],
-  ["pool.bitnovex.com", "http://pool.bitnovex.com:8133"],
+  ["superblockchain.con-ip.com/btnx", "https://superblockchain.con-ip.com:8013"],
+  ["pool.bitnovex.com", "http://pool.bitnovex.com:8233"],
 
  ]
 };
 
 var networkStat2 = {
   "btnx": [
-  ["superblockchain.con-ip.com/btnx", "https://superblockchain.con-ip.com:8887"],
-	["pool.bitnovex.com", "http://pool.bitnovex.com:8133"]
+  ["superblockchain.con-ip.com/btnx", "https://superblockchain.con-ip.com:8013"],
+  ["pool.bitnovex.com", "http://pool.bitnovex.com:8233"]
  ]
 };
